df6a2214195102c2cc88425c0bb4aa29168c1657
Roman Zastavnyi <roma.zastavnyi@gmail.com>
2019-07-04 16:48:34 +0300

    modify register modal window

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -1,8 +1,58 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Registration</h1>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md6>
+        <v-card class="elevation-12">
+          <v-toolbar dark>
+            <v-toolbar-title>Registration form</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form v-model="valid" ref="form" validation>
+              <v-text-field 
+                prepend-icon="person"
+                name="name" 
+                label="Name" 
+                type="text"
+                v-model="name"
+                :rules="[ v => !!v || 'Name is required' ]"
+              ></v-text-field>
+              <v-text-field 
+                prepend-icon="person"
+                name="email" 
+                label="Email" 
+                type="text"
+                v-model="email"
+                :rules="emailRules"
+              ></v-text-field>
+              <v-text-field 
+                prepend-icon="lock" 
+                name="password" 
+                label="Password" 
+                type="password"
+                :counter="6"
+                v-model="password"
+                :rules="passwordRules"
+              ></v-text-field>
+              <v-text-field 
+                prepend-icon="lock" 
+                name="confirm-password" 
+                label="Confirm password" 
+                type="password"
+                :counter="6"
+                v-model="confirmPassword"
+                :rules="confirmPasswordRules"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn 
+              dark
+              @click="onSubmit"
+              :disabled="!valid"
+            >Create account</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -10,8 +60,39 @@
 
 <script>
 export default {
-  data: () => ({
-    
-  })
+  data () {
+    return {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      valid: false,
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => (v && v.length >= 6) || 'Password must be equal or more than 6 characters'
+      ],
+      confirmPasswordRules: [
+        v => !!v || 'Password is required',
+        v => (!!v && v === this.password) || 'Password should match'
+      ]
+    }
+  },
+  methods: {
+    onSubmit () {
+      if (!this.$refs.form.validate()) {
+        return
+      }
+      const formData = {
+        name: this.name,
+        email: this.email,
+        password: this.password
+      }
+      console.log('submit', formData)
+    }
+  }
 }
 </script>
