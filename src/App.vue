87201a8daddc64b2972421a66e3a9654b6096bb2
Roman Zastavnyi <roma.zastavnyi@gmail.com>
2019-07-04 14:21:56 +0300

    add header and drawer

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,62 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+    <v-navigation-drawer 
+      app 
+      temporary
+      v-model="drawer"
+    >
+      <v-list>
+        <v-list-tile
+          v-for="link in links"
+          :key="link.item"
+          :to="link.url"
+        >
+          <v-list-tile-action>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-tile-action>
 
+          <v-list-tile-content>
+            <v-list-tile-title v-text="link.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app dark>
+      <v-toolbar-side-icon
+        class="hidden-md-and-up"
+        @click="drawer = !drawer"
+      ></v-toolbar-side-icon>
+      <v-toolbar-title>Sales MASTER</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn 
+          flat
+          v-for="link in links"
+          :key="link.title"
+          :to="link.url"
+        >
+          <v-icon left>{{ link.icon }}</v-icon>
+          {{ link.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
-    }
-  }
+  data: () => ({
+    drawer: false,
+    links: [
+      { title: 'Login', icon: 'lock', url: '/login' },
+      { title: 'Registration', icon: 'face', url: '/registration' },
+      { title: 'Orders', icon: 'bookmark_border', url: '/orders' },
+      { title: 'New ad', icon: 'note_add', url: '/new' },
+      { title: 'My ad', icon: 'list', url: '/list' }
+    ]
+  })
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
-import './plugins/vuetify'
 import App from './App.vue'
+import Vuetify from 'vuetify/lib'
+import router from './router';
+import 'vuetify/src/stylus/app.styl'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
